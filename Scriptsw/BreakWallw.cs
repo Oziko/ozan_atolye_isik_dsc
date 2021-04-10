ddb4cbdc10be494c4071049dd3e209dd3ad2de66
Ozan Etuzsoy <ozanetuzsoy@gmail.com>
2021-04-10 21:48:25 +0300

    Add files via upload

---
+++ Scriptsw/BreakWallw.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakWallw : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("Duvara carpti.");
+            BreakTheWall();
+        }
+    }
+
+    void BreakTheWall()
+    {
+        transform.GetComponent<BoxCollider>().enabled = false;
+
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<Rigidbody>().isKinematic = false;
+            child.GetComponent<Rigidbody>().AddForce(Vector3.forward, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Scriptsw/PlayerControllerw.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerControllerw : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    int count; //TMPRO İÇİN
+    public TextMeshProUGUI countText, winText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0; //TMPRO İÇİN
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    //COLLECTABLE'LARI DİZDİKTEN SONRA
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            other.gameObject.SetActive(false);
+            count++; //TMRO İÇİN
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 7)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
